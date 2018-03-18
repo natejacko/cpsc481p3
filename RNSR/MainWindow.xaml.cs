8a4dbc3a2f4b54b7ec957fcf246ecfe798b07a60
natejacko <njacko@shaw.ca>
2018-03-18 10:44:56 -0600

    Added header/footer functionality, switching between screens, pay screen actually having the necessary buttons and other minor changes

--- RNSR/MainWindow.xaml.cs
+++ RNSR/MainWindow.xaml.cs
@@ -23,6 +23,88 @@ namespace RNSR
         public MainWindow()
         {
             InitializeComponent();
+            Map.Visibility = Visibility.Hidden;
+            ViewOrder.Visibility = Visibility.Hidden;
+            Menu.Visibility = Visibility.Hidden;
+            Pay.Visibility = Visibility.Hidden;
+            ItemList.Visibility = Visibility.Hidden;
+            HeaderFooter.Visibility = Visibility.Hidden;
+            LoginScreen.Visibility = Visibility.Visible;
+        }
+
+        private void ResetButtons()
+        {
+            MapButton.Background = new SolidColorBrush(Color.FromRgb(122, 121, 121));
+            ViewOrderButton.Background = new SolidColorBrush(Color.FromRgb(122, 121, 121));
+            MenuButton.Background = new SolidColorBrush(Color.FromRgb(122, 121, 121));
+            PayButton.Background = new SolidColorBrush(Color.FromRgb(122, 121, 121));
+            MoreButton.Background = new SolidColorBrush(Color.FromRgb(122, 121, 121));
+        }
+
+        private void HideAllScreensAfterLogin()
+        {
+            Map.Visibility = Visibility.Hidden;
+            ViewOrder.Visibility = Visibility.Hidden;
+            Menu.Visibility = Visibility.Hidden;
+            Pay.Visibility = Visibility.Hidden;
+            ItemList.Visibility = Visibility.Hidden;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginScreen.Visibility = Visibility.Hidden;
+            Map.Visibility = Visibility.Visible;
+            HeaderFooter.Visibility = Visibility.Visible;
+            MapButton.Background = new SolidColorBrush(Color.FromRgb(135, 40, 40));
+            HeaderScreenName.Text = "Floor Map";
+        }
+
+        private void MapButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.ResetButtons();
+            MapButton.Background = new SolidColorBrush(Color.FromRgb(135, 40, 40));
+            HeaderScreenName.Text = "Floor Map";
+            this.HideAllScreensAfterLogin();
+            Map.Visibility = Visibility.Visible;
+        }
+
+        private void ViewOrderButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.ResetButtons();
+            ViewOrderButton.Background = new SolidColorBrush(Color.FromRgb(135, 40, 40));
+            HeaderScreenName.Text = "View Order";
+            this.HideAllScreensAfterLogin();
+            ViewOrder.Visibility = Visibility.Visible;
+            ItemList.Visibility = Visibility.Visible;
+        }
+
+        private void MenuButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.ResetButtons();
+            MenuButton.Background = new SolidColorBrush(Color.FromRgb(135, 40, 40));
+            HeaderScreenName.Text = "Add Items to Order";
+            this.HideAllScreensAfterLogin();
+            Menu.Visibility = Visibility.Visible;
+            ItemList.Visibility = Visibility.Visible;
+        }
+
+        private void PayButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.ResetButtons();
+            PayButton.Background = new SolidColorBrush(Color.FromRgb(135, 40, 40));
+            HeaderScreenName.Text = "Pay for Order";
+            this.HideAllScreensAfterLogin();
+            Pay.Visibility = Visibility.Visible;
+            ItemList.Visibility = Visibility.Visible;
+        }
+
+        private void MoreButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.ResetButtons();
+            MoreButton.Background = new SolidColorBrush(Color.FromRgb(135, 40, 40));
+            HeaderScreenName.Text = "More Options";
+            this.HideAllScreensAfterLogin();
+            More.Visibility = Visibility.Visible;
         }
     }
 }
